a5018659a783db50f84d5fe2c69185caf6de674f
Farsana2004 <133860295+Farsana2004@users.noreply.github.com>
2026-09-15 22:12:45 +0530

    first commit
    just created

---
+++ task2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[5];
+        System.out.println("Enter 5 numbers:");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        for (int i=0;i<numbers.length;i++){
+            for(int j=0;j<numbers.length-i-1;j++){
+                if(numbers[j]>numbers[j+1]){
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j+1];
+                    numbers[j+1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted numbers:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " "); 
+        }
+
+        int secondlowest = numbers[1];
+        System.out.println("\nSecond lowest number: " + secondlowest);
+
+        int secondhighest = numbers[3];
+        System.out.println("Second highest number: " + secondhighest);
+    }
+}
